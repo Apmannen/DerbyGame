aaadf9df2adb1dc46ecd414e05761cc8f0081283
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-08-01 17:03:13 +0200

    #2; Introduced interact dependecy

--- Assets/0_Shellander/Scripts/Interact/SgInteractable.cs
+++ Assets/0_Shellander/Scripts/Interact/SgInteractable.cs
@@ -1,9 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SgInteractable : SgBehavior
 {
+	public SgObjectDependency[] dependencies;
+
 	private SgInteractGroup m_InteractGroup;
 	public SgInteractGroup InteractGroup => SgUtil.LazyParentComponent(this, ref m_InteractGroup);
+	private BoxCollider2D m_Collider;
+	private BoxCollider2D Collider => SgUtil.LazyComponent(this, ref m_Collider);
+
+	private void Update()
+	{
+		foreach(SgObjectDependency dependency in dependencies)
+		{
+			Collider.enabled = dependency.gameObject.activeSelf;
+		}
+	}
+}
+
+[System.Serializable]
+public class SgObjectDependency
+{
+	public GameObject gameObject;
 }
