91df57953cd1466ac584f1b45ad2f1721822a33f
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-08-02 23:39:42 +0200

    #8; Set camera scroll bounds

--- Assets/0_Shellander/Scripts/SgCamera.cs
+++ Assets/0_Shellander/Scripts/SgCamera.cs
@@ -3,23 +3,29 @@ using UnityEngine;
 public class SgCamera : MonoBehaviour
 {
     public Camera cam;
+	public bool isStatic = true;
+	public Transform boundsLeft;
+	public Transform boundsRight;	
 
     private SgPlayer m_Player;
 
-    public void AttachPlayer(SgPlayer player)
+	public void AttachPlayer(SgPlayer player)
     {
         m_Player = player;    
 	}
 
 	private void Update()
 	{
-		if(m_Player == null)
+		if(isStatic || m_Player == null)
 		{
 			return;
 		}
 
 		Vector3 position = cam.transform.position;
 		position.x = m_Player.transform.position.x;
-		cam.transform.position = position;
+		if(position.x < boundsRight.position.x && position.x > boundsLeft.position.x)
+		{
+			cam.transform.position = position;
+		}		
 	}
 }
