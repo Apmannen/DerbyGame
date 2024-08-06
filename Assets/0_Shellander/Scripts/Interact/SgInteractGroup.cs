a7e744b5afe3c4b57199460fc0aa8d3c2a9de805
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-08-07 00:49:05 +0200

    #13; Preperations for trigger interaction to change room

--- Assets/0_Shellander/Scripts/Interact/SgInteractGroup.cs
+++ Assets/0_Shellander/Scripts/Interact/SgInteractGroup.cs
@@ -1,8 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public enum SgInteractType { Illegal, Walk, Look, Use, Pickup, Talk, Wait }
+public enum SgInteractType { Illegal, Walk, Look, Use, Pickup, Talk, Wait, Collision }
 
 public class SgInteractGroup : SgBehavior
 {
