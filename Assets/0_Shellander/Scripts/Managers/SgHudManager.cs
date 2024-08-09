ce80ad88108d40dbbe8e4bfe25b9c33f7a26fff3
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-08-09 20:16:28 +0200

    #17; Wheel background

--- Assets/0_Shellander/Scripts/Managers/SgHudManager.cs
+++ Assets/0_Shellander/Scripts/Managers/SgHudManager.cs
@@ -6,10 +6,19 @@ public class SgHudManager : MonoBehaviour
 	public SgUiCursor cursor;
 	public SgItembar itembar;
 	public SgWeaponWheel weaponWheel;
+	public CanvasGroup wheelBgGroup;
+	public float wheelBgAlphaSmoothTime = 0.1f;
+
+	private float m_BgAlphaVel = 0;
 
 	private void Awake()
 	{
 		weaponWheel.AddEventCallback(OnWheelEvent);
+	}
+
+	private void Update()
+	{
+		wheelBgGroup.alpha = Mathf.SmoothDamp(wheelBgGroup.alpha, IsWheelVisible ? 1 : 0, ref m_BgAlphaVel, wheelBgAlphaSmoothTime);
 	}
 
 	private void OnWheelEvent(SgWeaponWheelEvent wheelEvent)
