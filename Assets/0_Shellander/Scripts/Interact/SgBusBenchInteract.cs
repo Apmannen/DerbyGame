e46b0a97a382b8e7292ef715ecb0c28191be95ed
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-08-13 19:35:13 +0200

    #27; Cleanup

--- Assets/0_Shellander/Scripts/Interact/SgBusBenchInteract.cs
+++ Assets/0_Shellander/Scripts/Interact/SgBusBenchInteract.cs
@@ -7,29 +7,19 @@ public class SgBusBenchInteract : SgInteractGroup
 	public int busCardMissingTranslationId;
 	public SgRoomName goToRoom = SgRoomName.Illegal;
 
-	private bool CutsceneAborted => m_AbortClicks >= 2;
-	private int m_AbortClicks = 0;
-
 	public override IEnumerator InteractRoutine(SgPlayer player, SgInteractType interactType)
 	{
 		if (interactType == SgInteractType.Use)
 		{
-			m_AbortClicks = 0;
-
 			player.SetStance(SgPlayerStance.Sitting);
 			bus.gameObject.SetActive(true);
 			yield return bus.AnimateStep(0);
-			//bus.StartAnimation();
-			//yield return Wait(5);
-			//CheckHandleAborted();
-			//float busLeaveTime = 3;
 
 			if (ItemManager.IsCollected(SgItemType.BussCard))
 			{
 				player.SetStance(SgPlayerStance.Hidden);
 				yield return bus.AnimateStep(1);
 				yield return bus.AnimateStep(2);
-				//yield return Wait(busLeaveTime);
 				SceneManager.SetRoom(goToRoom);
 			}
 			else
@@ -37,8 +27,6 @@ public class SgBusBenchInteract : SgInteractGroup
 				player.SetStance(SgPlayerStance.Normal);
 				yield return bus.AnimateStep(1);
 				yield return bus.AnimateStep(2);
-				//yield return Wait(busLeaveTime);
-				//CheckHandleAborted();
 				yield return player.character.Talk(busCardMissingTranslationId);
 			}			
 		}
@@ -47,25 +35,4 @@ public class SgBusBenchInteract : SgInteractGroup
 			yield return base.InteractRoutine(player, interactType);
 		}
 	}
-
-	
-
-	//private void CheckHandleAborted()
-	//{
-	//	if(!CutsceneAborted)
-	//	{
-	//		return;
-	//	}
-	//	bus.gameObject.SetActive(false);
-	//}
-
-	//private IEnumerator Wait(float maxDuration)
-	//{
-	//	float time = 0;
-	//	while (time < maxDuration && !CutsceneAborted)
-	//	{
-	//		time += Time.deltaTime;
-	//		yield return null;
-	//	}
-	//}
 }
