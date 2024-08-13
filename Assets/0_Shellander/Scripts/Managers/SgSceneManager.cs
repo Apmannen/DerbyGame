42bafecec6102ec498536efb994c34bed4c25a18
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-08-13 20:12:10 +0200

    #26; Added room name

--- Assets/0_Shellander/Scripts/Managers/SgSceneManager.cs
+++ Assets/0_Shellander/Scripts/Managers/SgSceneManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum SgRoomName { Illegal, Home, Stockholm, Solna, ApartmentBuilding, Sewers, Shop }
+public enum SgRoomName { Illegal, Home, Stockholm, Solna, ApartmentBuilding, Sewers, Shop, Apartment }
 
 public class SgSceneManager : MonoBehaviour
 {
