92653fb1513743da528a86eb02b3cad12b3035e4
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-08-15 02:29:10 +0200

    #32; Make sure list exist

--- Assets/0_Shellander/Scripts/Managers/SgEventManager.cs
+++ Assets/0_Shellander/Scripts/Managers/SgEventManager.cs
@@ -8,56 +8,42 @@ public class SgEventManager : MonoBehaviour
 {
 	private readonly Dictionary<SgEventName, List<SgEvent>> m_Listeners = new();
 
-	//private void Awake()
-	//{
-	//	var eventNames = Enum.GetValues(typeof(SgEventName));
-	//	foreach (SgEventName eventName in eventNames)
-	//	{
-	//		m_Listeners[eventName] = new List<SgEvent>();
-	//	}
-	//}
-
-	private void AddIfMissing(SgEventName eventName)
+	private List<SgEvent> GetEventList(SgEventName eventName)
 	{
-		if(!m_Listeners.ContainsKey(eventName))
+		if (!m_Listeners.ContainsKey(eventName))
 		{
 			m_Listeners[eventName] = new();
 		}
+		return m_Listeners[eventName];
 	}
 
 	public void Register(SgEventName eventName, Action action)
 	{
-		AddIfMissing(eventName);
-		m_Listeners[eventName].Add(new SgEvent { action0 = action });
+		GetEventList(eventName).Add(new SgEvent { action0 = action });
 	}
 	public void Register<T>(SgEventName eventName, Action<T> action)
 	{
-		AddIfMissing(eventName);
-		m_Listeners[eventName].Add(new SgEvent { action1 = action });
+		GetEventList(eventName).Add(new SgEvent { action1 = action });
 	}
 	public void Unregister(SgEventName eventName, Action action)
 	{
-		AddIfMissing(eventName);
-		m_Listeners[eventName].RemoveAll(e => action.Equals(e.action0));
+		GetEventList(eventName).RemoveAll(e => action.Equals(e.action0));
 	}
 	public void Unregister<T>(SgEventName eventName, Action<T> action)
 	{
-		AddIfMissing(eventName);
-		m_Listeners[eventName].RemoveAll(e => action.Equals(e.action1));
+		GetEventList(eventName).RemoveAll(e => action.Equals(e.action1));
 	}
 
 	public void Execute(SgEventName eventName)
 	{
-		AddIfMissing(eventName);
-		foreach (SgEvent anEvent in m_Listeners[eventName])
+		foreach (SgEvent anEvent in GetEventList(eventName))
 		{
 			anEvent.action0?.Invoke();
 		}
 	}
 	public void Execute<T>(SgEventName eventName, T param)
 	{
-		AddIfMissing(eventName);
-		foreach (SgEvent anEvent in m_Listeners[eventName])
+		foreach (SgEvent anEvent in GetEventList(eventName))
 		{
 			if(anEvent.action1 != null)
 			{
