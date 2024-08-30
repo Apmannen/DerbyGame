86951ad4e8202bf634811f160aa347f5855eca5a
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-08-30 22:29:21 +0200

    #57; Set min x for cursor text

--- Assets/0_Shellander/Scripts/Ui/SgAutoInFrameUi.cs
+++ Assets/0_Shellander/Scripts/Ui/SgAutoInFrameUi.cs
@@ -4,6 +4,7 @@ public class SgAutoInFrameUi : MonoBehaviour
 {
 	public bool checkScreen = true;
 	public float maxY;
+	public float minX;
 
 	private Vector3 m_DefaultLocalPos;
 
@@ -16,6 +17,21 @@ public class SgAutoInFrameUi : MonoBehaviour
 	{
 		transform.localPosition = m_DefaultLocalPos;
 
+		AdjustMinX();
+		AdjustY();
+	}
+
+	private void AdjustMinX()
+	{
+		float compareMinX = minX;
+		if (transform.position.x < compareMinX)
+		{
+			SgUtil.SetPos(transform, compareMinX, 0);
+		}
+	}
+
+	private void AdjustY()
+	{
 		float compareMaxY = checkScreen ? Screen.height : maxY;
 		if (transform.position.y > compareMaxY)
 		{
