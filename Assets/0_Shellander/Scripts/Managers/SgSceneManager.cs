b0e41cf79e857d46d168d7ae86d3325db981169a
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-09-04 05:49:03 +0200

    #74; Created SewersRoom

--- Assets/0_Shellander/Scripts/Managers/SgSceneManager.cs
+++ Assets/0_Shellander/Scripts/Managers/SgSceneManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum SgRoomName { Illegal, Home, Stockholm, Solna, ApartmentBuilding, Sewers, Shop, Apartment, Bathroom, Pub }
+public enum SgRoomName { Illegal, Home, Stockholm, Solna, ApartmentBuilding, Sewers, Shop, Apartment, Bathroom, Pub, SewersRoom }
 
 public class SgSceneManager : SgBehavior
 {
